40d3316e140437b383c51a3662a3dac6a9b624f3
Renaaad3 <rinadno@gmail.com>
2024-09-29 15:23:53 +0300

    Initial Commit

---
+++ Team6PresentMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Team6PresentMe
+//
+//  Created by Renad Alotaibi on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Team6PresentMe/Team6PresentMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Team6PresentMeApp.swift
+//  Team6PresentMe
+//
+//  Created by Renad Alotaibi on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Team6PresentMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

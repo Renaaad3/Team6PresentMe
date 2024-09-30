84d9cf3631f7d4b228b5c0bbb2ed40b7fdc8c2a6
Renaaad3 <rinadno@gmail.com>
2024-09-30 17:01:24 +0300

    page 2

--- Team6PresentMe/Renad1Nahed.swift
+++ Team6PresentMe/Renad1Nahed.swift
@@ -9,8 +9,77 @@ import SwiftUI
 
 struct Renad1Nahed: View {
     @State private var isSelected = false
+    @State private var height: Int = 170
+    @State private var weight: Int = 50
+    
     var body: some View {
         VStack {
+            
+            VStack {
+                
+              Text("Enter Your Body Size").font(.largeTitle)
+                    .multilineTextAlignment(.center) .underline(true , color:Color.gray).baselineOffset(15)  .padding()
+
+                ZStack {
+                    VStack {
+                        
+                        Stepper {Text("Weight \(weight)")
+                        } onIncrement: { weight += 1
+                        } onDecrement: {weight -= 1
+                        }
+                
+                        
+                                  .font(.headline)
+                                  .padding(.bottom, 0)
+                                  .frame(maxWidth: .infinity, alignment: .leading)
+
+                        Slider(value: Binding(
+                                                   get: { Double(weight) },
+                                                   set: { weight = Int($0) }
+                                               ), in: 30...200, step: 1)
+                                               .accentColor(.blue)
+                          }
+                          .padding()
+                          .frame(height: 103.17)
+                          .frame(width: 355.17)
+                          .background(Color.white) // Background of the VStack
+                          .cornerRadius(10) // Rounded corners for the VStack
+                          .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 2)
+                      }
+                    
+                      .padding(.top, 30)
+                // Adjust the top padding as needed
+                  }
+            ZStack {
+                      VStack {
+                          Stepper {Text("Height \(height)")
+                          } onIncrement: { height += 1
+                          } onDecrement: {height -= 1
+                          }
+                          
+                          .font(.headline)
+                          .padding(.bottom, 0)
+                          .frame(maxWidth: .infinity, alignment: .leading)
+
+                          Slider(value: Binding(
+                                                     get: { Double(height) },
+                                                     set: { height = Int($0) }
+                                                 ), in: 30...200, step: 1)
+                                                 .accentColor(.blue)
+                      }
+                      .padding()
+                      .frame(height: 103.17)
+                      .frame(width: 355.17)
+                      .background(Color.white) // Background of the VStack
+                      .cornerRadius(10) // Rounded corners for the VStack
+                      .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 2)
+                  }
+                
+            Spacer()
+                            
+                                .padding(.bottom)
+                                .padding(.trailing, 30)
+                            
             Text("")
                 .font(.largeTitle)
                 .padding()
@@ -24,12 +93,14 @@ struct Renad1Nahed: View {
                             .onTapGesture {
                                 isSelected.toggle()
                             }
+
+            
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(red: 245/255, green: 245/255, blue: 247/255))
     }
+    
 }
 #Preview {
     Renad1Nahed()
 }
-

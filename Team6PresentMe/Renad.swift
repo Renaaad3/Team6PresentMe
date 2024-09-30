171dab9ec7d2a46fc62e791e3ff2b3f335e641ee
Renaaad3 <rinadno@gmail.com>
2024-09-30 17:55:26 +0300

    page 2 card size

--- Team6PresentMe/Renad.swift
+++ Team6PresentMe/Renad.swift
@@ -12,12 +12,12 @@ import SwiftUI
 struct Renad: View {
     @State private var selectedCard: Int? = nil // Track selected card
 
-        let bodyShapes = ["Round Shape", "Pear Shape", "Hourglass Shape", "Rectangle Shape", "Inverted Triangle Shape"]
+        let bodyShapes = ["  Round Shape", "  Pear Shape", "  Hourglass Shape", "  Rectangle Shape", "  Inverted Triangle Shape"]
 
         var body: some View {
             ScrollView { // Add ScrollView to make the page scrollable
                 VStack {
-                    Text("  Choose Your Body Shape")
+                    Text("  Choose Your Body Shape ")
                         .font(.largeTitle)
                         .font(.headline)
                         .fixedSize()
@@ -52,12 +52,13 @@ struct BodyShapeCard: View {
         VStack {
             Text(title)
                 .font(.headline)
-                .padding()
+                .padding(.bottom, 0)
+                .frame(maxWidth: .infinity, alignment: .leading)
         }
-        .frame(width: 398, height: 275) // Set specific width and height
+        .frame(width: 370, height: 275) // Set specific width and height
         .background(isSelected ? Color.white : Color.white) // Change color when selected
         .cornerRadius(10)
-        .shadow(color: Color.black.opacity(0.1), radius: 10, x: 5, y: 7)
+        .shadow(color: Color.black.opacity(0.1), radius: 10, x: 0, y: 7)
         .overlay(
             RoundedRectangle(cornerRadius: 10)
                 .stroke(isSelected ? Color.black : Color.white, lineWidth: 2) // Border color

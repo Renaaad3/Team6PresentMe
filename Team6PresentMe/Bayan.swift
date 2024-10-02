bb1de70fa93b2b7996506e04bac5fec060d12af3
Renaaad3 <rinadno@gmail.com>
2024-10-02 15:07:54 +0300

    page link

--- Team6PresentMe/Bayan.swift
+++ Team6PresentMe/Bayan.swift
@@ -2,8 +2,9 @@ import SwiftUI
 
 struct Bayan: View {
     let categories = ["Our fits", "Shirts", "Jeans", "Dress"]
+    var selectedImage: String? = nil
     
-        let images = ["ourfits_image", "shirts_image", "jeans_image", "dress_image"]
+    let images = ["ourfits_image", "shirts_image", "jeans_image", "dress_image"]
     @State private var selectedCategory: String = "Main Page"
     
     let adaptiveColumn = [GridItem(.adaptive(minimum: 165), spacing: 20)]
@@ -19,103 +20,106 @@ struct Bayan: View {
                             .fill(Color.gray.opacity(0.3))
                             .frame(width: 165, height: 165)
                         
-                        Image("bodytype") // تأكد من إضافة هذه الصورة إلى Assets
-                            .resizable()
-                            .frame(width: 80, height: 150)
-                            .scaledToFit()
-                            .frame(width: 165, height: 165)
-                            .background(Color.white)
-                             .cornerRadius(10)
-                              .shadow(radius: 4, x: 0, y: 2)
-                        
-                    }
-                    
-                    VStack (spacing: 20){
-                        Text("Neture")
-                            .font(.largeTitle)
-                            .multilineTextAlignment(.leading)
-                            .font(.custom("American Typewriter", size: 18))
-
-                        
-                        // الألوان
-                        HStack (spacing: -10) {
-                            Circle()
-                                .fill(Color(red: 1.0, green: 0.9, blue: 0.85))
-                                .frame(width: 40, height: 40)
-                                .overlay(Circle().stroke(Color.gray))
+                        if let selectedImage = selectedImage {
                             
-                            Circle()
-                                .fill(Color(red: 0.95, green: 0.8, blue: 0.75))
-                                .frame(width: 40, height: 40)
-                                .overlay(Circle().stroke(Color.gray))
+                            Image(selectedImage)
+                                .resizable()
+                                .frame(width: 80, height: 150)
+                                .scaledToFit()
+                                .frame(width: 165, height: 165)
+                                .background(Color.white)
+                                .cornerRadius(10)
+                                .shadow(radius: 4, x: 0, y: 2)
                             
-                            Circle()
-                                .fill(Color(red: 0.9, green: 0.7, blue: 0.65))
-                                .frame(width: 40, height: 40)
-                                .overlay(Circle().stroke(Color.gray))
-                            
-                            Circle()
-                                .fill(Color(red: 0.8, green: 0.6, blue: 0.45))
-                                .frame(width: 40, height: 40)
-                                .overlay(Circle().stroke(Color.gray))
-                            
-                            Circle()
-                                .fill(Color(red: 0.4, green: 0.25, blue: 0.2))
-                                .frame(width: 40, height: 40)
-                                .overlay(Circle().stroke(Color.gray))
                         }
                     }
-                }
-                .padding(.horizontal)
-                
-                Divider()
-                
-                // عنوان الفئات
-                Text("Explore our categories")
-                    .font(.custom("American Typewriter", size: 18))
+                        
+                        VStack (spacing: 20){
+                            Text("Neture")
+                                .font(.largeTitle)
+                                .multilineTextAlignment(.leading)
+                                .font(.custom("American Typewriter", size: 18))
+                            
+                            
+                            // الألوان
+                            HStack (spacing: -10) {
+                                Circle()
+                                    .fill(Color(red: 1.0, green: 0.9, blue: 0.85))
+                                    .frame(width: 40, height: 40)
+                                    .overlay(Circle().stroke(Color.gray))
+                                
+                                Circle()
+                                    .fill(Color(red: 0.95, green: 0.8, blue: 0.75))
+                                    .frame(width: 40, height: 40)
+                                    .overlay(Circle().stroke(Color.gray))
+                                
+                                Circle()
+                                    .fill(Color(red: 0.9, green: 0.7, blue: 0.65))
+                                    .frame(width: 40, height: 40)
+                                    .overlay(Circle().stroke(Color.gray))
+                                
+                                Circle()
+                                    .fill(Color(red: 0.8, green: 0.6, blue: 0.45))
+                                    .frame(width: 40, height: 40)
+                                    .overlay(Circle().stroke(Color.gray))
+                                
+                                Circle()
+                                    .fill(Color(red: 0.4, green: 0.25, blue: 0.2))
+                                    .frame(width: 40, height: 40)
+                                    .overlay(Circle().stroke(Color.gray))
+                            }
+                        }
+                    }
                     .padding(.horizontal)
-                
-                // المربعات
-                ScrollView {
-                    LazyVGrid(columns: adaptiveColumn, spacing: 20) {
-                        ForEach(0..<categories.count, id: \.self) {
-      index in NavigationLink(destination: Sara()) {
-          
-                                VStack {
-                                    Image("ourfits_image")
-                                        .resizable()
-                                        .frame(width: 80, height: 140)
-                                        .scaledToFit()
-
-                                        
+                    
+                    Divider()
+                    
+                    // عنوان الفئات
+                    Text("Explore our categories")
+                        .font(.custom("American Typewriter", size: 18))
+                        .padding(.horizontal)
+                    
+                    // المربعات
+                    ScrollView {
+                        LazyVGrid(columns: adaptiveColumn, spacing: 20) {
+                            ForEach(0..<categories.count, id: \.self) {
+                                index in NavigationLink(destination: Sara()) {
                                     
-      Text("Our fits")
-      .font(.custom("American Typewriter", size: 18))
-      .foregroundColor(.black)
-       }
-      .frame(width: 165, height: 165)
-      .background(Color.white)
-       .cornerRadius(10)
-               .shadow(radius: 4, x: 0, y: 2)
-                                                                }
-              .simultaneousGesture(TapGesture().onEnded {
-                  selectedCategory = categories[index]
-              })
-                  }
-          }
-                    .padding()
+                                    VStack {
+                                        Image("ourfits_image")
+                                            .resizable()
+                                            .frame(width: 80, height: 140)
+                                            .scaledToFit()
+                                        
+                                        
+                                        
+                                        Text("Our fits")
+                                            .font(.custom("American Typewriter", size: 18))
+                                            .foregroundColor(.black)
+                                    }
+                                    .frame(width: 165, height: 165)
+                                    .background(Color.white)
+                                    .cornerRadius(10)
+                                    .shadow(radius: 4, x: 0, y: 2)
+                                }
+                                .simultaneousGesture(TapGesture().onEnded {
+                                    selectedCategory = categories[index]
+                                })
+                            }
+                        }
+                        .padding()
+                    }
+                    
+                    Spacer()
                 }
-                
-                Spacer()
+                .navigationTitle(selectedCategory)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color(red: 245/255, green: 245/255, blue: 247/255))
             }
-            .navigationTitle(selectedCategory)
-        }
         }
     }
-
-struct Bayan_Previews: PreviewProvider {
-    static var previews: some View {
-        Bayan()
+    struct Bayan_Previews: PreviewProvider {
+        static var previews: some View {
+            Bayan(selectedImage: "ourfits_image")
+        }
     }
-}
-
--- Team6PresentMe/Renad.swift
+++ Team6PresentMe/Renad.swift
@@ -1,10 +1,9 @@
-
 import SwiftUI
 
 struct Renad: View {
     @State private var selectedCard: Int? = nil
     
-    let bodyShapes = ["  Round Shape", " Hourglass Shape", " Rectangle Shape ", " Inverted Triangle Shape", "   Pear Shape"]
+    let bodyShapes = ["Round Shape", "Hourglass Shape", "Rectangle Shape", "Inverted Triangle Shape", "Pear Shape"]
     
     let bodyShapeImages = ["Round", "Hourglass", "Rectangle", "Inverted Tringle", "pear"]
     
@@ -13,13 +12,14 @@ struct Renad: View {
         "Characterized by a well-defined waist and balanced bust and hip measurements.",
         "A rectangle body means that the width of your bust, waist, and hips are approximately the same.",
         "Is an analogy that your bust and shoulders are broader than your hips.",
-        "Pear body shape is characterised by large hips which are wider than the bust and shoulders." ]
+        "Pear body shape is characterised by large hips which are wider than the bust and shoulders."
+    ]
     
     var body: some View {
-        NavigationStack {
+        NavigationView {
             ScrollView {
                 VStack {
-                    Text("  Choose Your Body Shape! ")
+                    Text("Choose Your Body Shape!")
                         .font(.custom("American Typewriter", size: 30)).bold()
                         .padding(.bottom, 0)
                         .fixedSize()
@@ -38,29 +38,31 @@ struct Renad: View {
                                 description: bodyShapeDescriptions[index]
                             )
                             .onTapGesture {
-                                selectedCard = index // Set selected card
+                                selectedCard = index
                             }
-                            .padding(.horizontal, 17) // Padding for x position
+                            .padding(.horizontal, 17)
                         }
                     }
-                    .padding(.top, 10) // Padding for y position
+                    .padding(.top, 10)
                     
                     Spacer()
                     HStack {
                         Spacer()
-                        NavigationLink(destination: Bayan()) {
-                            Image(systemName: "arrow.right.circle.fill")
-                                .resizable()
-                                .frame(width: 50, height: 50)
-                                .foregroundColor(.black)
+                        if let selectedCard = selectedCard {
+                            // Pass the selected image to the Bayan view
+                            NavigationLink(destination: Bayan(selectedImage: bodyShapeImages[selectedCard])) {
+                                Image(systemName: "arrow.right.circle.fill")
+                                    .resizable()
+                                    .frame(width: 50, height: 50)
+                                    .foregroundColor(.black)
+                            }
+                            .padding(.bottom, 30)
+                            .padding(.trailing, 20)
                         }
-                        .padding(.bottom, 30)
-                        .padding(.trailing, 20)
                     }
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                 .background(Color(red: 245/255, green: 245/255, blue: 247/255))
-               
                 .padding()
             }
         }
@@ -104,11 +106,8 @@ struct BodyShapeCard: View {
                     .stroke(isSelected ? Color.black : Color.white, lineWidth: 2)
             )
         }
-       
     }
-    
 }
-
 #Preview {
     Renad()
 }
